eb41ddc162721079958b0f7c8180b14e6516d931
_cheily <cheiily.pm@gmail.com>
2024-01-21 22:37:25 +0100

    namespaces - recycling

--- Assets/Scripts/Recykling/DragController.cs
+++ Assets/Scripts/Recykling/DragController.cs
@@ -1,88 +1,88 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class DragController : MonoBehaviour
-{
-
-    public Draggable LastDragged => lastDragged;
-    private bool isDragActive = false;
-    private Vector2 screenPosition;
-    private Vector3 worldPosition;
-
-    private Draggable lastDragged;
-    private void Awake()
+namespace Recykling {
+    public class DragController : MonoBehaviour
     {
-        DragController[] dragControllers = FindObjectsOfType<DragController>();
-        if (dragControllers.Length > 1)
-        {
-            Destroy(gameObject);
-        }
-    }
-    void Update()
-    {
-        if(isDragActive && (Input.GetMouseButtonUp(0) || (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended)))
-        {
-            Drop();
-            return;
-        }
-        if (Input.GetMouseButton(0))
-        {
-            Vector3 mousePosition = Input.mousePosition;
-            screenPosition = new Vector2(mousePosition.x, mousePosition.y);
-        }
-        else if (Input.touchCount > 0)
-        {
-            screenPosition = Input.GetTouch(0).position;
-        }
-        else return;
 
-        worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
-        if (isDragActive) 
-        {            
-            Drag();
-        }
-        else
+        public Draggable LastDragged => lastDragged;
+        private bool isDragActive = false;
+        private Vector2 screenPosition;
+        private Vector3 worldPosition;
+
+        private Draggable lastDragged;
+        private void Awake()
         {
-            RaycastHit2D hit = Physics2D.Raycast(worldPosition, Vector2.zero);
-            if (hit.collider != null)
+            DragController[] dragControllers = FindObjectsOfType<DragController>();
+            if (dragControllers.Length > 1)
             {
-                Draggable draggable = hit.collider.GetComponent<Draggable>();
-                if (draggable != null)
+                Destroy(gameObject);
+            }
+        }
+        void Update()
+        {
+            if(isDragActive && (Input.GetMouseButtonUp(0) || (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended)))
+            {
+                Drop();
+                return;
+            }
+            if (Input.GetMouseButton(0))
+            {
+                Vector3 mousePosition = Input.mousePosition;
+                screenPosition = new Vector2(mousePosition.x, mousePosition.y);
+            }
+            else if (Input.touchCount > 0)
+            {
+                screenPosition = Input.GetTouch(0).position;
+            }
+            else return;
+
+            worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+            if (isDragActive)
+            {
+                Drag();
+            }
+            else
+            {
+                RaycastHit2D hit = Physics2D.Raycast(worldPosition, Vector2.zero);
+                if (hit.collider != null)
                 {
-                    lastDragged = draggable;
-                    InitDrag();
+                    Draggable draggable = hit.collider.GetComponent<Draggable>();
+                    if (draggable != null)
+                    {
+                        lastDragged = draggable;
+                        InitDrag();
+                    }
                 }
             }
         }
-    }
 
-    void InitDrag() 
-    { 
-        lastDragged.lastPosition = lastDragged.transform.position;
-        UpdateDragStatus(true);
-    }
-
-    void Drag()
-    {
-        lastDragged.transform.position = new Vector2(worldPosition.x, worldPosition.y);
-    }
-
-    void Drop()
-    {
-        UpdateDragStatus(false);
-    }
-
-    void UpdateDragStatus(bool isDragging)
-    {
-        isDragActive = lastDragged.isDragging = isDragging;
-        if (isDragging)
+        void InitDrag()
         {
-            lastDragged.gameObject.layer = 7;
+            lastDragged.lastPosition = lastDragged.transform.position;
+            UpdateDragStatus(true);
         }
-        else
+
+        void Drag()
         {
-            lastDragged.gameObject.layer = 0;
+            lastDragged.transform.position = new Vector2(worldPosition.x, worldPosition.y);
+        }
+
+        void Drop()
+        {
+            UpdateDragStatus(false);
+        }
+
+        void UpdateDragStatus(bool isDragging)
+        {
+            isDragActive = lastDragged.isDragging = isDragging;
+            if (isDragging)
+            {
+                lastDragged.gameObject.layer = 7;
+            }
+            else
+            {
+                lastDragged.gameObject.layer = 0;
+            }
         }
     }
 }
--- Assets/Scripts/Recykling/PointsDisplayer.cs
+++ Assets/Scripts/Recykling/PointsDisplayer.cs
@@ -1,26 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 
-public class PointsDisplayer : MonoBehaviour
-{
-    public RecyklingGameScript rgs;
-    public TMP_Text pointsText;
-    // Start is called before the first frame update
-    void Start()
+namespace Recykling {
+    public class PointsDisplayer : MonoBehaviour
     {
+        public RecyklingGameScript rgs;
+        public TMP_Text pointsText;
+        // Start is called before the first frame update
+        void Start()
+        {
         
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-           UpdatePoints();
-    }
+        // Update is called once per frame
+        void Update()
+        {
+            UpdatePoints();
+        }
 
-    public void UpdatePoints()
-    {
-        pointsText.text = "Points: " + rgs.pointsCounter.GetPoints();
+        public void UpdatePoints()
+        {
+            pointsText.text = "Points: " + rgs.pointsCounter.GetPoints();
+        }
     }
 }
