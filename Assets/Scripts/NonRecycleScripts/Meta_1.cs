d6e79ad83bc22f57e1abbf0508ce19938371429f
_cheily <cheiily.pm@gmail.com>
2024-01-22 01:30:29 +0100

    some fixes for the build (incomplete)

--- Assets/Scripts/NonRecycleScripts/Meta_1.cs
+++ Assets/Scripts/NonRecycleScripts/Meta_1.cs
@@ -8,8 +8,7 @@ namespace NonRecycleScripts {
         public  GameObject destroyObject;
         public GameObject destroyObject2;
         // Update is called once per frame
-        private void OnTriggerEnter2D(Collider2D collision)
-        {
+        private void OnTriggerEnter2D(Collider2D collision) {
             Destroy(destroyObject);
             Destroy(destroyObject2);
             MiniGameStatus.Instance.SetStatus("non-recycle", statistics.getScore(), true);
